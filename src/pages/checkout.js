320223efc68f74601fc198c3cd9d27531e59303b
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-06-02 19:47:34 +0200

    First roundtrip of Stripe complete

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,3 +1,5 @@
+import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
 import { useSession } from 'next-auth/client';
 import Image from 'next/image';
 import Currency from 'react-currency-formatter';
@@ -5,11 +7,29 @@ import { useSelector } from 'react-redux';
 import CheckoutProduct from '../components/checkout/CheckoutProduct';
 import Header from '../components/Header';
 import { selectItems, selectTotal } from '../slices/basketSlice';
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 const Checkout = () => {
   const basketItems = useSelector(selectItems);
   const [session] = useSession();
   const subTotal = useSelector(selectTotal);
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    // create checkout session from my own backend
+    const checkoutSession = await axios.post('/api/create-checkout-session', {
+      items: basketItems,
+      email: session.user.email,
+    });
+
+    // redirect to stripe checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
 
   return (
     <div className='bg-gray-100'>
@@ -50,6 +70,8 @@ const Checkout = () => {
               </h2>
 
               <button
+                onClick={createCheckoutSession}
+                role='link'
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
